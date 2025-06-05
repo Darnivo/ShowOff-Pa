6b6009429049a8ceb5ebca653bb0ba117a5d9822
nad154 <119729778+nad154@users.noreply.github.com>
2025-06-05 14:48:59 +0200

    bird box

---
+++ Assets/Nad's Storage/Scripts/BirdKey.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class BirdKey : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (CompareTag("Sight"))
+        {
+            
+        }
+    }
+}
