674d34c617486506aae55269c33dea670ca5d9fd
nad154 <119729778+nad154@users.noreply.github.com>
2025-07-04 13:24:18 +0200

    text trigger fix

--- Assets/Nad's Storage/Scripts/TextTrigger.cs
+++ Assets/Nad's Storage/Scripts/TextTrigger.cs
@@ -7,6 +7,7 @@ public class TextTrigger : MonoBehaviour
 {
     public TextMeshPro text;
     public float fadeDuration = 0.2f;
+    private bool isOn = false; 
     void Awake()
     {
         Color c = text.color;
@@ -17,20 +18,25 @@ public class TextTrigger : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            fadeInText();
+            Debug.Log("player enter"); 
+            if(isOn == false) StartCoroutine(fadeInText());
         }
     }
 
-    private void fadeInText()
+    private IEnumerator fadeInText()
     {
         float elapsedTime = 0f;
         Color c = text.color;
         while (elapsedTime < fadeDuration)
         {
             elapsedTime += Time.deltaTime;
-            float alpha = Mathf.Lerp(0, 1, elapsedTime / fadeDuration);
+            float alpha = Mathf.Clamp01(elapsedTime / fadeDuration);
             c.a = alpha;
-            text.color = c; 
+            text.color = c;
+            yield return null;
         }
+        c.a = 1;
+        text.color = c;
+        isOn = true; 
     }
 }
