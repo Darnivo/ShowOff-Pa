7e27a02b7d9ce42fe1c6ac59e0538a96af80b188
nad154 <119729778+nad154@users.noreply.github.com>
2025-07-04 22:26:07 +0200

    small fixes to things

--- Assets/Nad's Storage/Scripts/DissolveEffect.cs
+++ Assets/Nad's Storage/Scripts/DissolveEffect.cs
@@ -6,22 +6,22 @@ public class DissolveEffect : MonoBehaviour
 {
     private Material objMaterial;
     private Coroutine currentCoroutine;
-    public bool startInvisible = false; 
+    public bool startInvisible = false;
     private void Awake()
     {
         objMaterial = GetComponent<Renderer>().material;
-        if(!startInvisible) objMaterial.SetFloat("_DissolveAmount", 0f);
+        if (!startInvisible) objMaterial.SetFloat("_DissolveAmount", 0f);
     }
 
     public void dissolveIn(float duration)
     {
         StartDissolve(1f, 0f, duration);
-        
+
     }
     public void dissolveOut(float duration)
     {
         StartDissolve(0f, 1f, duration);
-        
+
     }
     private void StartDissolve(float startValue, float endValue, float duration)
     {
@@ -44,6 +44,18 @@ public class DissolveEffect : MonoBehaviour
             yield return null;
         }
         objMaterial.SetFloat("_DissolveAmount", endValue);
+    }
+
+    public void callDissolveInAfterDelay(float duration)
+    {
+        StartCoroutine(dissolveInAfterDelay(0.3f, duration));
+    }
+
+    private IEnumerator dissolveInAfterDelay(float delayTime, float duration)
+    {
+        yield return new WaitForSeconds(delayTime);
+        dissolveIn(duration);
+
     }
 
 }
